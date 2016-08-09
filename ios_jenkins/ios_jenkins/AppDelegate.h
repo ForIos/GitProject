04ad4446add4f5bb5852b097ed28815e56a8982c
USER <894143788@qq.com>
2016-08-09 23:08:25 +0800

    demodemo
    dd

---
+++ ios_jenkins/ios_jenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios_jenkins
+//
+//  Created by USER on 16/8/9.
+//  Copyright © 2016年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios_jenkins/ios_jenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios_jenkins
+//
+//  Created by USER on 16/8/9.
+//  Copyright © 2016年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
